dddab8282f9f2439dcff4ad465002e1b9abc488c
Michael Clark <michaelclarknashville@protonmail.com>
2020-04-11 17:19:03 -0500

    refactored, much dryer

--- stocks.py
+++ stocks.py
@@ -15,25 +15,40 @@ for purchase in purchases:
         if company == purchase[0]:
             print(f"I purchased {stockDict[company]} stock for ${purchase[1] * purchase[3]}")
 
-purchase_summary = {
+## ------------
 
-}
+purchaseSet = set()
+
+for purchase in purchases:
+    purchaseSet.add(purchase[0])
+
+for company in purchaseSet:
+    print(f'-----{company}-----')
+    total = 0
+    for purchase in purchases:
+        if purchase[0] == company:
+            total += (purchase[1] * purchase[3])
+            print(f'{purchase[1]} shares at {purchase[3]} dollars each on {purchase[2]}')
+    print(f'Total value of stock portfolio: ${total}') 
 
 
-for company in stockDict:
-    purchase_summary[company] = list()
-    for a, b, c, d in purchases:
-        if company == a:
-            purchase_summary[company].append([b, c, d])
+# purchase_set = 
+# for purchase in purchases:
     
-for company in purchase_summary:
-    print(f"-----{company}-----")
-    total_value = 0
-    for array in purchase_summary[company]:
-        total_value += (array[0] * array[2])
-        print(f'{array[0]} shares at {array[2]} dollars each on {array[1]}')
-    if total_value != 0:
-        print(f'Total value of stock portfolio: ${total_value}')
+# for company in stockDict:
+#     purchase_summary[company] = list()
+#     for a, b, c, d in purchases:
+#         if company == a:
+#             purchase_summary[company].append([b, c, d])
+    
+# for company in purchase_summary:
+#     print(f"-----{company}-----")
+#     total_value = 0
+#     for array in purchase_summary[company]:
+#         total_value += (array[0] * array[2])
+#         print(f'{array[0]} shares at {array[2]} dollars each on {array[1]}')
+#     if total_value != 0:
+#         print(f'Total value of stock portfolio: ${total_value}')
 
     
         
